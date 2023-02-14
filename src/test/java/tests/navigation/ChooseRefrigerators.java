e33f55d5c35da232dea37fe711e19ae032092ec3
Olena <elenamelnik1856@gmail.com>
2023-02-14 16:39:38 +0200

    Rebased TSB-33 from dev

---
+++ src/test/java/tests/navigation/ChooseRefrigerators.java
@@ -0,0 +1,21 @@
+package tests.navigation;
+
+import base.AbstractBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AlloUaHomePageElements;
+
+public class ChooseRefrigerators extends AbstractBaseTest {
+    @Test
+    public void chooseRefrigerators(){
+        AlloUaHomePageElements allo = new AlloUaHomePageElements(driver);
+        GoToAllo();
+        allo.getCatalog().click();
+        allo.moveOnCategoryHousehold();
+        allo.getRefrigerators().click();
+
+        Assert.assertTrue(getUrl().contains("ua/holodilniki/"));
+        Assert.assertTrue(allo.getProductRefrigerators().getSize().height>1);
+
+    }
+}
