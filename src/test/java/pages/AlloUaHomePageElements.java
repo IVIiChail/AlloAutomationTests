964c226177503e79aa806b77de0c5e47e6cf3bee
Olena <elenamelnik1856@gmail.com>
2023-02-14 10:59:44 +0200

    add elements, add test file

--- src/test/java/pages/AlloUaHomePageElements.java
+++ src/test/java/pages/AlloUaHomePageElements.java
@@ -1,8 +1,11 @@
 package pages;
 
 import base.AbstractBasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class AlloUaHomePageElements extends AbstractBasePage {
     public AlloUaHomePageElements(WebDriver driver) {
@@ -12,6 +15,12 @@ public class AlloUaHomePageElements extends AbstractBasePage {
     private static final String DROPDOWN_GUARANTY_RETURN_A = "//div[contains(@class,'dropdown')]//a[contains(@href,'warranty_and_service')]";
 
     public static final String FOR_CUSTOMERS_DROPDOWN_A = "//a[contains(text(),'Покупцям')]";
+    public static final String HEADER_CITY_BTN ="//button[@data-geo-label='Київ']";
+    public static final String HEADER_SWITCH_CITY = "//a[@href='https://allo.ua/ua/dnipro']";
+    public static final String HEADER_STORES_BTN ="(//a[@href='https://allo.ua/ua/dnipro/offline_stores/'])[1]";
+    public static final String LOC_LABEL="//span[@class ='mh-loc__label']";
+    public static final String CONTENT_TITLE_H1="//h1[@class ='content__title']";
+    public static final String TD_ADDRESS ="//td[@class ='table-body__address']']";
 
     public WebElement getGuaranteeReturnBtn(){
         return waitUntilElementToBeClickable(DROPDOWN_GUARANTY_RETURN_A);
@@ -20,6 +29,30 @@ public class AlloUaHomePageElements extends AbstractBasePage {
     public WebElement getForCustomersDropdownBtn(){
         return waitUntilElementToBeClickable(FOR_CUSTOMERS_DROPDOWN_A);
     }
+    public WebElement getCityButton(){
+        return waitUntilElementToBeClickable(HEADER_CITY_BTN);
+    }
+
+    public WebElement getDnipro(){
+        return waitUntilElementToBeClickable(HEADER_SWITCH_CITY);
+    }
+    public WebElement getStores(){
+        return waitUntilElementToBeClickable(HEADER_STORES_BTN);
+    }
+    public WebElement getLocLabel(){
+        return waitUntilElementToBeClickable(LOC_LABEL);
+    }
+    public WebElement getContentTitleH1(){
+        return waitUntilElementToBeClickable(CONTENT_TITLE_H1);
+    }
+    public List<WebElement> getTdAddress(){
+        return waitUntilElementsToBeClickable(TD_ADDRESS);
+    }
+
+
+
 
 
 }
+
+
---
+++ src/test/java/tests/navigation/GoToShopWithDniproCity.java
@@ -0,0 +1,26 @@
+package tests;
+
+import base.AbstractBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AlloUaHomePageElements;
+
+public class GoToShopWithDniproCity extends AbstractBaseTest {
+@Test
+    public void GoToStores(){
+        AlloUaHomePageElements allo = new AlloUaHomePageElements(driver);
+        driver.navigate();
+        GoToAllo();
+        sleep(3);
+        allo.getCityButton().click();
+        allo.getDnipro().click();
+        allo.waitUntilUrlContainsText("https://allo.ua/ua/dnipro/");
+        allo.getStores().click();
+
+    Assert.assertTrue(getUrl().contains("dnipro/offline_stores/"));
+    Assert.assertEquals(allo.getContentTitleH1().getText(),"Адреса магазинів АЛЛО у Дніпрі");
+    Assert.assertTrue(allo.getLocLabel().isEnabled());
+    Assert.assertTrue(allo.getTdAddress().size()>1);
+    //розмір це наявність таких елементів на сторінці
+    }
+}
