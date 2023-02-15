c5f6b9db3e4e8b3cee18b140f3adeff683b60ad8
Olena <elenamelnik1856@gmail.com>
2023-02-15 11:02:10 +0200

    Add test file, add elements

--- src/test/java/pages/AlloUaHomePageElements.java
+++ src/test/java/pages/AlloUaHomePageElements.java
@@ -14,6 +14,9 @@ public class AlloUaHomePageElements extends AbstractBasePage {
 
     private static final String DROPDOWN_GUARANTY_RETURN_A = "//div[contains(@class,'dropdown')]//a[contains(@href,'warranty_and_service')]";
     public static final String FOR_CUSTOMERS_DROPDOWN_A = "//a[contains(text(),'Покупцям')]";
+    public static final String HEADER_BUYERS_BTN ="//a[@class='mh-button mh-button--open']";
+    public static final String BUY_PARTLY ="(//a[@href='https://allo.ua/ua/help/oplata-chastami/'])[1]";
+    public static final String CONTENT_TITLE_H2="//h2[@class='sp-page-title sp-h2 custom-header']";
     public static final String HEADER_CITY_BTN ="//button[@data-geo-label='Київ']";
     public static final String HEADER_SWITCH_CITY = "//a[@href='https://allo.ua/ua/dnipro']";
     public static final String HEADER_STORES_BTN ="(//a[@href='https://allo.ua/ua/dnipro/offline_stores/'])[1]";
@@ -32,8 +35,18 @@ public class AlloUaHomePageElements extends AbstractBasePage {
     public WebElement getGuaranteeReturnBtn(){
         return waitUntilElementToBeClickable(DROPDOWN_GUARANTY_RETURN_A);
     }
+
     public WebElement getForCustomersDropdownBtn(){
         return waitUntilElementToBeClickable(FOR_CUSTOMERS_DROPDOWN_A);
+    }
+    public WebElement getBuyers(){
+        return waitUntilElementToBeClickable(HEADER_BUYERS_BTN);
+    }
+    public WebElement getBuyPartly(){
+        return waitUntilElementToBeClickable(BUY_PARTLY);
+    }
+    public WebElement getContentTitleH2(){
+        return waitUntilElementToBeClickable(CONTENT_TITLE_H2);
     }
     public WebElement getCityButton(){
         return waitUntilElementToBeClickable(HEADER_CITY_BTN);
---
+++ src/test/java/tests/navigation/GoToPaymentPartly.java
@@ -0,0 +1,20 @@
+package tests.navigation;
+
+import base.AbstractBaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AlloUaHomePageElements;
+
+public class GoToPaymentPartly extends AbstractBaseTest {
+    @Test
+    public void goToPaymentPartly(){
+        AlloUaHomePageElements allo = new AlloUaHomePageElements(driver);
+        GoToAllo();
+        allo.getBuyers().click();
+        allo.getBuyPartly().click();
+        allo.waitUntilUrlContainsText("oplata-chastami/");
+
+        Assert.assertTrue(getUrl().contains("oplata-chastami/"));
+        Assert.assertEquals(allo.getContentTitleH2().getText(),"Обирайте товар та сплачуйте його частинами");
+    }
+}
