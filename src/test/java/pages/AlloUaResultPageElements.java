649d645c861b635e382c9e6bb561599106bd66a8
Michail Krivov <miharol@ukr.net>
2023-02-15 19:56:47 +0200

    TSB-42 passed test and refactore elements rebased

--- src/test/java/pages/AlloUaResultPageElements.java
+++ src/test/java/pages/AlloUaResultPageElements.java
@@ -11,13 +11,15 @@ public class AlloUaResultPageElements extends AbstractBasePage {
         super(driver);
     }
 
-    public static final String GUARANTEE_AND_SERVICE_PAGE_TITLE_H4 = "//h4[contains(@class,'page-head')]";
-    public static final String HEADER_BLOG_ALLO_LOGO_IMG = "//div[contains(@class,'header_menu')]//img[contains(@alt, 'Блог АЛЛО')]";
-    public static final String LIST_OF_PRODUCTS_TITLES_A = "//a[contains(@class,'product-card__title')]";
-    public static final String BICYCLES_A = "//li[contains(@class,'portal')]//a[contains(text(),'Велосипеди')]";
-    public static final String SMARTPHONES_AND_PHONES_NOKIA_A = "//li[contains(@class,'portal-card')]//a[contains(text(),'Nokia')]";
-    public static final String PLAYPENS_A = "//li[contains(@class,'portal')]//a[contains(text(),'Манежі')]";
-    public static final String GET_RESAULT_LINKALLOEXCHANHE = "//div[@class='mh-links']//a[@href='https://allo.ua/ua/tradein/']";
+    private static final String GUARANTEE_AND_SERVICE_PAGE_TITLE_H4 = "//h4[contains(@class,'page-head')]";
+    private static final String HEADER_BLOG_ALLO_LOGO_IMG = "//div[contains(@class,'header_menu')]//img[contains(@alt, 'Блог АЛЛО')]";
+    private static final String LIST_OF_PRODUCTS_TITLES_A = "//a[contains(@class,'product-card__title')]";
+    private static final String BICYCLES_A = "//li[contains(@class,'portal')]//a[contains(text(),'Велосипеди')]";
+    private static final String SMARTPHONES_AND_PHONES_NOKIA_A = "//li[contains(@class,'portal-card')]//a[contains(text(),'Nokia')]";
+    private static final String PLAYPENS_A = "//li[contains(@class,'portal')]//a[contains(text(),'Манежі')]";
+    private static final String APPSTORE_ALLO_APP_TITLE = "//h1[contains(@class,'product-header__title')]";
+    private static final String GET_RESAULT_LINKALLOEXCHANHE = "//div[@class='mh-links']//a[@href='https://allo.ua/ua/tradein/']";
+
     public WebElement getGuaranteeAndServicePageTitle(){
         return waitUntilElementToBeClickable(GUARANTEE_AND_SERVICE_PAGE_TITLE_H4);
     }
@@ -35,6 +37,9 @@ public class AlloUaResultPageElements extends AbstractBasePage {
     }
     public WebElement getPlaypensBtn() {
         return waitUntilElementToBeClickable(PLAYPENS_A);
+    }
+    public WebElement getAppStoreAlloTitle( ) {
+        return waitUntilElementToBeClickable(APPSTORE_ALLO_APP_TITLE);
     }
     public WebElement getResault_LinkAlloExchenge() {return waitUntilVisibilityOfElementLocated(GET_RESAULT_LINKALLOEXCHANHE);}
 }
--- src/test/java/tests/footer/GoToDownloadFromAppStore.java
+++ src/test/java/tests/footer/GoToDownloadFromAppStore.java
@@ -1,12 +1,24 @@
 package tests.footer;
 
 import base.AbstractBaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.AlloUaHomePageElements;
+import pages.AlloUaHomePageHelper;
+import pages.AlloUaResultPageElements;
 
 public class GoToDownloadFromAppStore extends AbstractBaseTest {
 
     @Test
     private void goToDownloadFromAppStore(){
-        
+        AlloUaHomePageHelper alloUaHomePageHelper = new AlloUaHomePageHelper(driver);
+        alloUaHomePageHelper.goToHomePage();
+
+        alloUaHomePageHelper.getDownloadWithAppStoreBtn().click();
+        AlloUaResultPageElements alloUaResultPageElements = new AlloUaResultPageElements(driver);
+        alloUaResultPageElements.goToNextTab(2);
+
+        Assert.assertTrue(getUrl().contains("https://apps.apple.com/ua/app"));
+        Assert.assertTrue(alloUaResultPageElements.getAppStoreAlloTitle().getText().contains("Алло: Тицяй Що Хочеш"));
     }
 }
