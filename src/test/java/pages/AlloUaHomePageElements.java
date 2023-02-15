2e4e48114348c6b6bc5947fcaf44d5a998c3f4b9
Olena <elenamelnik1856@gmail.com>
2023-02-15 11:02:10 +0200

    Add test file, add elements

--- src/test/java/pages/AlloUaHomePageElements.java
+++ src/test/java/pages/AlloUaHomePageElements.java
@@ -12,6 +12,9 @@ public class AlloUaHomePageElements extends AbstractBasePage {
     private static final String DROPDOWN_GUARANTY_RETURN_A = "//div[contains(@class,'dropdown')]//a[contains(@href,'warranty_and_service')]";
 
     public static final String FOR_CUSTOMERS_DROPDOWN_A = "//a[contains(text(),'Покупцям')]";
+    public static final String HEADER_BUYERS_BTN ="//a[@class='mh-button mh-button--open']";
+    public static final String BUY_PARTLY ="(//a[@href='https://allo.ua/ua/help/oplata-chastami/'])[1]";
+    public static final String CONTENT_TITLE_H2="//h2[@class='sp-page-title sp-h2 custom-header']";
 
     public WebElement getGuaranteeReturnBtn(){
         return waitUntilElementToBeClickable(DROPDOWN_GUARANTY_RETURN_A);
@@ -20,6 +23,14 @@ public class AlloUaHomePageElements extends AbstractBasePage {
     public WebElement getForCustomersDropdownBtn(){
         return waitUntilElementToBeClickable(FOR_CUSTOMERS_DROPDOWN_A);
     }
-
+    public WebElement getBuyers(){
+        return waitUntilElementToBeClickable(HEADER_BUYERS_BTN);
+    }
+    public WebElement getBuyPartly(){
+        return waitUntilElementToBeClickable(BUY_PARTLY);
+    }
+    public WebElement getContentTitleH2(){
+        return waitUntilElementToBeClickable(CONTENT_TITLE_H2);
+    }
 
 }
