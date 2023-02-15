60626f1213be188f0299ee8f0fd2bf14f087f2e7
Michail Krivov <miharol@ukr.net>
2023-02-15 19:23:57 +0200

    TSB-42 initialization test

---
+++ src/test/java/tests/footer/GoToDownloadFromAppStore.java
@@ -0,0 +1,12 @@
+package tests.footer;
+
+import base.AbstractBaseTest;
+import org.testng.annotations.Test;
+
+public class GoToDownloadFromAppStore extends AbstractBaseTest {
+
+    @Test
+    private void goToDownloadFromAppStore(){
+        
+    }
+}
